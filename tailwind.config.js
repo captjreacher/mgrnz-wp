b032071af1650918435b18ae229e9532a25d41e7
Mike <mike@mikerobinson.co.nz>
2025-11-03 23:30:10 +1300

    local

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.md",
+    "./layouts/**/*.html",
+    "./themes/congo/layouts/**/*.html",
+    "./themes/congo/assets/**/*.js",
+    "./themes/congo/assets/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
